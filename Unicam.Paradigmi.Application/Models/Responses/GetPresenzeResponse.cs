62ac76645e7da6f519d9edbf5ba4783721e09478
nicos <nicolo.stefani@studenti.unicam.it>
2024-03-28 10:45:22 +0100

    Aggiunta PresenzeResponse

---
+++ Unicam.Paradigmi.Application/Models/Responses/GetPresenzeResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unicam.Paradigmi.Application.Models.Dtos;
+
+namespace Unicam.Paradigmi.Application.Models.Responses
+{
+    public class GetPresenzeResponse
+    {
+        public List<PresenzaDto> Presenze { get; set; } = new List<PresenzaDto>();
+
+        public int NumeroPagine { get; set; }
+    }
+}
